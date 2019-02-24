daa777ed0c67a0ac1ca9c028f577d9f1126bb5cc
asamii <kian_samii@intuit.com>
2019-02-24 01:46:17 -0800

    Adding js file

---
+++ Kobold.club Roll20 Linker.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Kobold.club Roll20 Linker
+// @namespace    none
+// @version      0.1
+// @description  Turn Kobold Club names to links to roll20.
+// @author       RankWeis
+// @match        https://*kobold.club/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+
+
+
+function replace() {
+    'use strict';
+    const roll20 = "https://roll20.net/compendium/dnd5e/";
+    console.log("TamperMonkey - Replacing kobold.club names with links to roll20");
+    console.log(document.getElementsByClassName("group-info--header")[0].textContent);
+    console.log(Array.from(document.getElementsByClassName("monster-table--name ng-binding")));
+    Array.from(document.getElementsByClassName("monster-table--name ng-binding"))
+	.map(it => {
+        console.log("TamperMonkey - Doing it");
+		var elem = it.textContent;
+		var url = encodeURI(roll20 + elem);
+		it.innerHTML = `<a href="${url}">${elem}</a>`;
+	})
+}
+
+(function() {
+    waitForKeyElements(".monster-table--name", replace);
+})();
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # kobold-roll20-links
 Kobold Tampermonkey Utility
+
+# Usage
+
+[Install Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+
+Once installed, click on the extension and click 'Dashboard' and choose one of the methods of installation below.
+
+## Option 1: Trusting## 
+
+If you trust that I'm not going to be messing with you, and you want any updates I may have for this script, click 'Utilities' and copy the github url and paste it into the box that says URL. 
+
+## Option 2: Suspicious##
+
+There's no reason to trust to trust a random internet dude, so click the tab that says [+], then go to the github URL, vet the code to make sure I'm doing nothing suspicious, and paste it into the code box. Click File, then save, and enjoy!
+
+
+# Notes
+
+There are some monsters that don't appear on roll20, but will be linked to it anyway. I'm sorry that I'm too lazy to figure out a way to exclude those, but I also have faith that you can copy the name and paste it into google yourself!
