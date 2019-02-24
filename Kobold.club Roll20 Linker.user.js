16a50a02779519bda75e10c5573d56eda4f00245
asamii <kian_samii@intuit.com>
2019-02-24 02:40:42 -0800

    Adding ability to change current monsters.

--- Kobold.club Roll20 Linker.user.js
+++ Kobold.club Roll20 Linker.user.js
@@ -10,21 +10,34 @@
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        none
 // ==/UserScript==
+const roll20 = "https://roll20.net/compendium/dnd5e/";
 
-
-
-function replace() {
-    'use strict';
-    const roll20 = "https://roll20.net/compendium/dnd5e/";
-    console.log("TamperMonkey - Replacing kobold.club names with links to roll20");
-    Array.from(document.getElementsByClassName("monster-table--name ng-binding"))
-	.map(it => {
-		var elem = it.textContent;
-		var url = encodeURI(roll20 + elem);
-		it.innerHTML = `<a href="${url}">${elem}</a>`;
-	})
+function replaceCurrent() {
+    Array.from(document.getElementsByClassName("current-encounter--monster-name"))
+        .map(it => {
+                var elem = it.textContent;
+                var url = encodeURI(roll20 + elem);
+                it.innerHTML = `<a href="${url}">${elem}</a>`;
+        })
 }
 
+function replaceTable() {
+    'use strict';
+    console.log("TamperMonkey - Replacing kobold.club names with links to roll20");
+    Array.from(document.getElementsByClassName("monster-table--name ng-binding"))
+        .map(it => {
+                var elem = it.textContent;
+                var url = encodeURI(roll20 + elem);
+                it.innerHTML = `<a href="${url}">${elem}</a>`;
+        })
+    Array.from(document.getElementsByClassName("btn btn-sm btn-success"))
+        .map(it => {
+                it.onclick = waitForKeyElements(".current-encounter--monster-name", replaceCurrent);
+                });
+
+}
+
+
 (function() {
-    waitForKeyElements(".monster-table--name", replace);
+    waitForKeyElements(".monster-table--name", replaceTable);
 })();
